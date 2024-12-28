bd686fb014a558a1347104774ce461f50f522e0e
Erik An <erian@c3r1s1.42wolfsburg.de>
2024-12-28 10:38:54 +0100

    pseudocode in test for t_line_container

--- tests/unittests/test_lexer.cpp
+++ tests/unittests/test_lexer.cpp
@@ -7,21 +7,34 @@ struct TestTokenizeParams {
 	t_token *want_tokens;
 };
 
+struct s_line_container {
+	char* line;
+	int pos;
+} t_line_container;
+
+t_tokenizer new_tokenizer(char* line) {
+	t_tokenizer t;
+	t.line = line;
+	t.pos = 0;
+	return t;
+}
+
 class TestTokenizer : public::testing::TestWithParam<TestTokenizeParams>{};
 
 TEST_P(TestTokenizer, firstTests) {
 	TestTokenizeParams params = GetParam();
-	t_token* tokens = tokenize(params.line);
-	EXPECT_EQ(tokens, params.want_tokens);
-}
-
-t_token *create_tokens(std::vector<std::string> contents, std::vector<int> types) {
-	EXPECT_EQ(contents.size(), types.size());
-	t_token* head = NULL;
-	for (size_t i = 0; i < contents.size(); i++) {
-
+	param.line
+	param.want_tokens
+	t_line_container lc = new_line_container(param.line);
+	iterate_over_want_tokens {
+		t_token got_token = get_next_token(&tokenizer);
+		EXPECT_EQUAL(got_token, want_token)
 	}
-	return NULL;
+	
+	t_token* tokens = tokenize(params.line);
+
+
+	EXPECT_EQ(tokens, params.want_tokens);
 }
 
 INSTANTIATE_TEST_SUITE_P(
