f915a5114db72ab2e47017c4a732a225409038e9
Kay Freyer <kfreyer@c2r3s4.42wolfsburg.de>
2025-01-07 21:20:23 +0100

    refactor tests for pipes

--- tests/unittests/test_parser_pipes.cpp
+++ tests/unittests/test_parser_pipes.cpp
@@ -1,17 +1,21 @@
 #include "test_main.hpp"
 
-void test_pipe_cases(t_test_script_node want, t_script_node *sn, t_dllist* tokens) {
+void compare_pipe_node(t_test_script_node want, t_script_node *sn) {
     ASSERT_NE(nullptr, sn);
     ASSERT_EQ(sn->node_type, PIPE_NODE);
     ASSERT_EQ(sn->num_children, 2);
-    ASSERT_NE(nullptr,sn->child1);
-    ASSERT_EQ(CMD_NODE, sn->child1->node_type);
+    ASSERT_EQ(want.pipe_node_childs[0].type, sn->child1->node_type);
     ASSERT_NE(nullptr,sn->child2);
-    ASSERT_EQ(CMD_NODE, sn->child2->node_type);
+    ASSERT_EQ(want.pipe_node_childs[1].type, sn->child1->node_type);
 
-    compare_cmd_node(want.pipe_node_childs[0], (t_cmd_node)sn->child1->node_data.cmd_node);
-    compare_cmd_node(want.pipe_node_childs[1], (t_cmd_node)sn->child2->node_data.cmd_node);
+    if (sn->child1->node_type == CMD_NODE)
+        compare_cmd_node(want.pipe_node_childs[0], (t_cmd_node)sn->child1->node_data.cmd_node);
+    if (sn->child2->node_type == CMD_NODE)
+        compare_cmd_node(want.pipe_node_childs[1], (t_cmd_node)sn->child2->node_data.cmd_node);
+}
 
+void test_pipe_cases(t_test_script_node want, t_script_node *sn, t_dllist* tokens) {
+    compare_pipe_node(want, sn);
     free_script_node(sn);
     ft_dllstclear(&tokens, free_token);
 }
@@ -144,5 +148,23 @@ INSTANTIATE_TEST_SUITE_P(
                                      new_test_script_node(CMD_NODE, new_test_cmd_node(new_token("echo", BUILTIN), {}, {new_redirection(1, OUT, "file.txt", LITERAL)}), {}, {}),
                                      new_test_script_node(CMD_NODE, new_test_cmd_node(new_token("ls", WORD), {new_argument("-a", LITERAL)}, {}), {}, {}),
                                  })}
+        // ParserTestParams{6,
+        //     PIPE_TEST,
+        //     {{
+        //             new_token("wc", WORD),
+        //                 new_token("|", PIPE),
+        //             new_token("echo", BUILTIN),
+        //             new_token("|", PIPE),
+        //             new_token("ls", WORD),
+        //             new_token(NULL, END_OF_FILE)
+        //         }},
+        //     new_test_script_node(PIPE_NODE, {}, {},
+        //                          {
+        //                              new_test_script_node(PIPE_NODE, {}, {}, {
+        //                                      new_test_script_node(CMD_NODE, new_test_cmd_node(new_token("wc", WORD), {}, {}), {}, {}),
+        //                                      new_test_script_node(CMD_NODE, new_test_cmd_node(new_token("ls", BUILTIN), {}, {}), {}, {})
+        //                                  }),
+        //                              new_test_script_node(CMD_NODE, new_test_cmd_node(new_token("ls", WORD), {new_argument("-a", LITERAL)}, {}), {}, {}),
+        //                          })}
         )
     );
