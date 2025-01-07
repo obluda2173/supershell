225fb6739ba37fa1664a3d9f18138797b95145a6
Kay Freyer <kfreyer@c2r7s2.42wolfsburg.de>
2025-01-07 14:28:13 +0100

    green, one step closer to tree structure

--- src/parser.c
+++ src/parser.c
@@ -12,6 +12,51 @@
 
 #include "parser.h"
 #include "libft.h"
+#include <unistd.h>
+
+
+static t_list	*create_and_add_argument(t_list **arguments, t_token *t)
+{
+	t_argument	*arg;
+	t_list		*tmp;
+
+	arg = extract_argument(t);
+	/* if (!arg) */
+	/* { */
+	/* 	ft_lstclear(script, free_script_node); */
+	/* 	return (NULL); */
+	/* } */
+	tmp = ft_lstnew(arg);
+	/* if (!tmp) */
+	/* { */
+	/* 	ft_lstclear(script, free_script_node); */
+	/* 	return (NULL); */
+	/* } */
+	ft_lstadd_back(arguments, tmp);
+	return (tmp);
+}
+
+
+t_script_node *new_fill_cmd_node(t_script_node *sn, t_dllist *tokens) {
+	t_token			cur;
+
+	cur = *(t_token *)(tokens->content);
+	if (cur.type != REDIRECT_OUT && cur.type != REDIRECT_IN && cur.type != REDIRECT_APPEND && cur.type != HERE_DOC)
+		tokens = tokens->next;
+
+	while (tokens)
+	{
+		cur = *(t_token *)(tokens->content);
+		if (cur.type == PIPE)
+			return sn;
+		if (cur.type == END_OF_FILE)
+			return sn;
+		if (!create_and_add_argument(&sn->node_data.cmd_node.arguments, tokens->content))
+			return (NULL);
+		tokens = tokens->next;
+	}
+	return sn;
+}
 
 t_list	*parse(t_dllist *tokens)
 {
@@ -46,12 +91,10 @@ t_list	*parse(t_dllist *tokens)
 			while (tokens->prev)
 				tokens = tokens->prev;
 			init_cmd_node(sn->child1, *(t_token*)tokens->content);
-			/* fill_cmd_node(script, tokens) */
-			tokens = tokens->next;
+			new_fill_cmd_node(sn->child1, tokens);
 
 			while (((t_token*)tokens->content)->type != PIPE)
 				tokens = tokens->next;
-
 			tokens = tokens->next;
 			init_cmd_node(sn->child2, *(t_token*)tokens->content);
 			return ft_lstnew(sn);
--- tests/unittests/test_parser_pipes.cpp
+++ tests/unittests/test_parser_pipes.cpp
@@ -33,23 +33,22 @@ INSTANTIATE_TEST_SUITE_P(
                                      {
                                          new_test_script_node(CMD_NODE, new_test_cmd_node(new_token("echo", BUILTIN), {}, {}), {}, {}),
                                          new_test_script_node(CMD_NODE, new_test_cmd_node(new_token("ls", WORD), {}, {}), {}, {})
+                                     })}},
+        ParserTestParams{
+            0, PIPE_TEST, {
+                {
+                    new_token("echo", BUILTIN),
+                    new_token("file.txt", WORD),
+                    new_token("|", PIPE),
+                    new_token("ls", WORD),
+                    new_token(NULL, END_OF_FILE)
+                }
+            },
+            {
+                new_test_script_node(PIPE_NODE, {}, {},
+                                     {
+                                     new_test_script_node(CMD_NODE, new_test_cmd_node(new_token("echo", BUILTIN), {new_argument("file.txt", LITERAL),}, {}), {}, {}),
+                                     new_test_script_node(CMD_NODE, new_test_cmd_node(new_token("ls", WORD), {}, {}), {}, {}),
                                      })}}
-        // ParserTestParams{
-        //     0, PIPE_TEST, {
-        //         {
-        //             new_token("echo", BUILTIN),
-        //             new_token("file.txt", WORD),
-        //             new_token("|", PIPE),
-        //             new_token("ls", WORD),
-        //             new_token("-a", WORD),
-        //             new_token(NULL, END_OF_FILE)
-        //         }
-        //     },
-        //     {
-        //         new_test_script_node(PIPE_NODE, {}, {},
-        //                              {
-        //                              new_test_script_node(CMD_NODE, new_test_cmd_node(new_token("echo", BUILTIN), {new_argument("file.txt", LITERAL),}, {}), {}, {}),
-        //                              new_test_script_node(CMD_NODE, new_test_cmd_node(new_token("ls", WORD), {new_argument("-a", LITERAL),}, {}), {}, {}),
-        //                              })}}
         )
     );
