ebe602f33ce46a94bc36062cfb2e571d013ce4f8
Kay Freyer <kfreyer@c2r3s4.42wolfsburg.de>
2025-01-07 17:39:08 +0100

    no more new_fill_cmd_node

--- src/parser.c
+++ src/parser.c
@@ -12,27 +12,7 @@
 
 #include "parser.h"
 #include "libft.h"
-
-t_script_node *new_fill_cmd_node(t_script_node *sn, t_dllist *tokens) {
-	t_token			cur;
-
-	cur = *(t_token *)(tokens->content);
-	if (cur.type != REDIRECT_OUT && cur.type != REDIRECT_IN && cur.type != REDIRECT_APPEND && cur.type != HERE_DOC)
-		tokens = tokens->next;
-
-	while (tokens)
-	{
-		cur = *(t_token *)(tokens->content);
-		if (cur.type == PIPE)
-			return sn;
-		if (cur.type == END_OF_FILE)
-			return sn;
-		if (!create_and_add_argument(sn, tokens->content))
-			return (NULL);
-		tokens = tokens->next;
-	}
-	return sn;
-}
+#include <unistd.h>
 
 t_script_node	*parse(t_dllist *tokens)
 {
@@ -59,13 +39,13 @@ t_script_node	*parse(t_dllist *tokens)
 			while (tokens->prev)
 				tokens = tokens->prev;
 			init_cmd_node(sn->child1, *(t_token*)tokens->content);
-			new_fill_cmd_node(sn->child1, tokens);
+			fill_cmd_node(sn->child1, tokens);
 
 			while (((t_token*)tokens->content)->type != PIPE)
 				tokens = tokens->next;
 			tokens = tokens->next;
 			init_cmd_node(sn->child2, *(t_token*)tokens->content);
-			new_fill_cmd_node(sn->child2, tokens);
+			fill_cmd_node(sn->child2, tokens);
 			return sn;
 		}
 		tokens = tokens->next;
