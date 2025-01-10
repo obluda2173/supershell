f679e0180d1ff5d211f389dce90bed1f338ffaef
Kay Freyer <kfreyer@c2r7s1.42wolfsburg.de>
2025-01-10 12:11:37 +0100

    refactor tests_executor

--- tests/unittests/test_executor.cpp
+++ tests/unittests/test_executor.cpp
@@ -1,9 +1,19 @@
 #include "test_main.hpp"
+#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 
-t_script_node *new_script_node() {
+struct ExecutorTestsParams {
+	const char* cmd;
+	int want_return;
+	const char* want_stdout;
+};
+
+class ExecutorTestSuite : public::testing::TestWithParam<ExecutorTestsParams>{};
+
+t_script_node *new_script_node(char *cmd) {
 	t_script_node *sn = (t_script_node*)malloc(sizeof(t_script_node));
 	sn->node_type = CMD_NODE;
-	sn->node_data.cmd_node.cmd_token = new_token(ft_strdup("random_cmd"), BUILTIN);
+	sn->node_data.cmd_node.cmd_token = new_token(ft_strdup(cmd), BUILTIN);
 	sn->node_data.cmd_node.arguments = NULL;
 	sn->node_data.cmd_node.redirections = NULL;
 	sn->upstream = NULL;
@@ -11,17 +21,29 @@ t_script_node *new_script_node() {
 	return sn;
 }
 
-TEST(ExecutorTests, TestBinaries) {
-	t_script_node *sn = new_script_node();
+TEST_P(ExecutorTestSuite, ErrorTests) {
+	ExecutorTestsParams params = GetParam();
+
+	(void)params;
+	t_script_node *script = new_script_node((char*)params.cmd);
 	testing::internal::CaptureStderr();
-	int got_return = execute_script(sn, NULL);
+	int got_return = execute_script(script, NULL);
 	EXPECT_EQ(127, got_return);
 	std::string got = testing::internal::GetCapturedStderr();
 	EXPECT_STREQ("Command not found: random_cmd\n", got.c_str());
 
-	free_script_node(sn);
+	free_script_node(script);
 }
 
+INSTANTIATE_TEST_SUITE_P(
+	ExecutorTests,
+	ExecutorTestSuite,
+	testing::Values(
+		ExecutorTestsParams{"random_cmd", 127, "Command not found: random_cmd\n"}
+// 		ExecutorTestParams{{new_argument("Hello", LITERAL)}, "Hello\n", 0},
+// 		ExecutorTestParams{{new_argument("Hello", LITERAL), new_argument("World", LITERAL)}, "Hello World\n", 0}
+		)
+	);
 
 // t_script_node *create_cmd_node(std::vector<t_argument> args) {
 // 	t_script_node *script = (t_script_node*)malloc(sizeof(t_script_node));
