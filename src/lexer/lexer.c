097f76e71da9fc7cc8317c72f94d63b383ec9940
Kay Freyer <kfreyer@c2r7s1.42wolfsburg.de>
2025-01-10 12:12:02 +0100

    MORE MORE MORE TESTSSSSgit add .git add .git add .

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,7 +6,7 @@
 /*   By: erian <erian@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 17:07:34 by erian             #+#    #+#             */
-/*   Updated: 2025/01/08 16:48:49 by erian            ###   ########.fr       */
+/*   Updated: 2025/01/10 10:10:13 by erian            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,11 +84,9 @@ t_dllist	*tokenize(char *line)
 	t_dllist	*tokens;
 
 	line = format_input(line);
-	//printf("\nLine: %s\n\n", line); //testing
 	if (!line)
 		return (NULL);
 	tokens = tokenize_line(line);
 	free(line);
-	//print_tokens(tokens); //testing
 	return (tokens);
 }
--- src/lexer/lexer_format_input_1.c
+++ src/lexer/lexer_format_input_1.c
@@ -1,16 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   format_input_1.c                                   :+:      :+:    :+:   */
+/*   lexer_format_input_1.c                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: erian <erian@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/25 10:33:18 by erian             #+#    #+#             */
-/*   Updated: 2025/01/09 09:43:22 by erian            ###   ########.fr       */
+/*   Updated: 2025/01/10 10:39:41 by erian            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+static char	*trim_spaces(char *str)
+{
+	const char *start;
+	const char *end;
+	size_t trimmed_length;
+	char *trimmed_str;
+
+	if (!str)
+		return (NULL);
+	end = str + ft_strlen(str) - 1;
+	start = str;
+	while (*start && ((*start >= 9  && *start <= 13) || *start == 32))
+		start++;
+	while (end > start && ((*end >= 9  && *end <= 13) || *end == 32))
+		end--;
+	trimmed_length = end - start + 1;
+	trimmed_str = malloc(trimmed_length + 1);
+	if (!trimmed_str)
+		return (NULL);
+	ft_strlcpy(trimmed_str, start, trimmed_length + 1);
+	free(str);
+	return (trimmed_str);
+}
 
 char	*format_input(char *input)
 {
@@ -27,7 +51,7 @@ char	*format_input(char *input)
 		return (NULL);
 	while (input[i])
 	{
-		if (isdigit(input[i]) && (input[i + 1] == '>'
+		if (ft_isdigit(input[i]) && (input[i + 1] == '>'
 				|| (input[i + 1] == '>' && input[i + 2] == '>')))
 			result = handle_redirection(input, &i, result, &j);
 		else if (input[i] == '<' && input[i + 1] == '<')
@@ -38,5 +62,5 @@ char	*format_input(char *input)
 			result = handle_operators(input, &i, result, &j);
 	}
 	result[j] = '\0';
-	return (result);
+	return (trim_spaces(result));
 }
