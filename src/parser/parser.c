506fd9a3fa71cdcea367b8c3453fc3909713e2ba
Kay Freyer <kfreyer@c2r8s1.42wolfsburg.de>
2025-01-14 12:28:21 +0100

    refactor/norm parser.c

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -20,10 +20,11 @@ t_dllist	*find_last_logical(t_dllist *tokens)
 	tokens = ft_dllstlast(tokens);
 	while (tokens->prev)
 	{
-		if (((t_token *)tokens->content)->type == RPAREN) {
+		if (((t_token *)tokens->content)->type == RPAREN)
+		{
 			while (tokens->prev && ((t_token *)tokens->content)->type != LPAREN)
 				tokens = tokens->prev;
-			continue;
+			continue ;
 		}
 		if (((t_token *)tokens->content)->type == AND
 			|| ((t_token *)tokens->content)->type == OR)
@@ -45,53 +46,67 @@ t_dllist	*find_last_pipe(t_dllist *tokens)
 	return (tokens);
 }
 
+bool	has_surrounding_parens(t_dllist *tokens)
+{
+	t_dllist	*scnd_to_last;
+	t_dllist	*head;
+	int			nbr_open_parens;
+
+	scnd_to_last = ft_dllstlast(tokens)->prev;
+	if (((t_token *)tokens->content)->type == LPAREN
+		&& ((t_token *)scnd_to_last->content)->type == RPAREN)
+	{
+		head = tokens->next;
+		nbr_open_parens = 1;
+		while (head != scnd_to_last)
+		{
+			if (((t_token *)head->content)->type == LPAREN)
+				nbr_open_parens++;
+			if (((t_token *)head->content)->type == RPAREN)
+				nbr_open_parens--;
+			if (nbr_open_parens == 0)
+				break ;
+			head = head->next;
+		}
+		if (head == scnd_to_last)
+			return (true);
+	}
+	return (false);
+}
+
+t_dllist	*remove_surrounding_parens(t_dllist *tokens)
+{
+	t_dllist	*scnd_to_last;
+	t_dllist	*head;
+
+	scnd_to_last = ft_dllstlast(tokens)->prev;
+	head = tokens;
+	tokens = head->next;
+	tokens->prev = NULL;
+	scnd_to_last->prev->next = scnd_to_last->next;
+	scnd_to_last->next->prev = scnd_to_last->prev;
+	scnd_to_last->next = NULL;
+	scnd_to_last->prev = NULL;
+	ft_dllstclear(&scnd_to_last, free_token);
+	return (tokens);
+}
+
 t_script_node	*parse(t_dllist *tokens)
 {
 	if (!tokens)
 		return (get_error_node("no tokens"));
 	if (((t_token *)ft_dllstlast(tokens)->content)->type != END_OF_FILE)
 		return (get_error_node("no end of file token"));
-
-	/* /\* remove surrounding parens *\/ */
-	t_dllist* scnd_to_last = ft_dllstlast(tokens)->prev;
-	bool remove = false;
-
-	/* printf("type first: %d\n", ((t_token *)tokens->content)->type); */
-	/* printf("type scnd_to_last: %d\n", ((t_token*)scnd_to_last->content)->type); */
-	if (((t_token *)tokens->content)->type == LPAREN && ((t_token*)scnd_to_last->content)->type == RPAREN) {
-
-		if (tokens->next == scnd_to_last) {
-			return get_error_node("parsing error near (");
-		}
-		t_dllist *head = tokens->next;
-		/* head = head->next; */
-		int nbr_open_parens = 1;
-		while (head != scnd_to_last) {
-			if (((t_token *)head->content)->type == LPAREN)
-				nbr_open_parens++;
-			if (((t_token *)head->content)->type == RPAREN)
-				nbr_open_parens--;
-			if (nbr_open_parens == 0)
-				break;
-			head = head->next;
-		}
-		if (head == scnd_to_last)
-			remove = true;
+	if (((t_token *)tokens->content)->type == LPAREN)
+	{
+		if (((t_token *)tokens->next->content)->type == RPAREN)
+			return (get_error_node("parsing error near ("));
 	}
-
-	if (remove) {
-		t_dllist *head = tokens;
-		tokens = head->next;
-		tokens->prev = NULL;
-
-		scnd_to_last->prev->next = scnd_to_last->next;
-		scnd_to_last->next->prev = scnd_to_last->prev;
-		scnd_to_last->next = NULL;
-		scnd_to_last->prev = NULL;
-		ft_dllstclear(&scnd_to_last, free_token);
-		return parse(tokens);
+	if (has_surrounding_parens(tokens))
+	{
+		tokens = remove_surrounding_parens(tokens);
+		return (parse(tokens));
 	}
-
 	tokens = find_last_logical(tokens);
 	if (((t_token *)tokens->content)->type == AND
 		|| ((t_token *)tokens->content)->type == OR)
