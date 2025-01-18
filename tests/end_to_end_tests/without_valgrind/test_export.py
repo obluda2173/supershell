dcf2c785c2bba1fd3fcdf8178d9821afac9b56be
Kay Freyer <kfreyer@c2r7s2.42wolfsburg.de>
2025-01-18 16:16:09 +0100

    add test for export

---
+++ tests/end_to_end_tests/without_valgrind/test_export.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+from conftest import (
+    start_process,
+    send_cmds_minishell_with_open_fds,
+    parse_out_and_err_minishell,
+    get_open_fds,
+)
+from assertions import (
+    assert_no_memory_error_fsanitize,
+    assert_no_new_file_descriptors,
+    assert_no_memory_error_fsanitize,
+)
+
+
+def assert_export_variables_same(stdout_minishell, stdout_bash):
+    assert len(stdout_bash) == len(stdout_minishell)
+
+    for out1 in stdout_minishell:
+        assert any(
+            [out1.split("=")[0] in line for line in stdout_bash]
+        ), f"{out1} not in {stdout_bash}"
+
+
+def test_export():
+    minishell = start_process("./minishell")
+    open_fds_beginning = get_open_fds()
+
+    cmd = ["export"]
+    cmd = "\n".join(cmd + ["echo $?\n"])
+
+    bash = start_process("bash")
+    assert bash.stdin is not None
+    stdout_bash, _ = bash.communicate(cmd.encode())
+    stdout_bash = stdout_bash.decode().split("\n")[:-1]  # cut empty line
+
+    stdout_minishell, stderr_minishell, open_fds_end = (
+        send_cmds_minishell_with_open_fds(minishell, cmd)
+    )
+
+    stdout_minishell, stderr_minishell = parse_out_and_err_minishell(
+        stdout_minishell, stderr_minishell
+    )
+
+    assert_no_memory_error_fsanitize(stdout_minishell, stderr_minishell)
+    assert_export_variables_same(stdout_minishell, stdout_bash)
+    assert "0" == stdout_minishell[-1]
+    assert len(stderr_minishell) == 0
+
+    assert_no_new_file_descriptors(open_fds_beginning, open_fds_end)
