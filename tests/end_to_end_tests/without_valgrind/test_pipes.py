9766de9e214f29b176facda475feed35ccdbc595
Kay Freyer <kfreyer@c1r6s4.42wolfsburg.de>
2025-01-27 13:21:56 +0100

    red test

--- tests/end_to_end_tests/without_valgrind/test_pipes.py
+++ tests/end_to_end_tests/without_valgrind/test_pipes.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import pexpect
-from conftest import cstm_expect, remove_cariage, remove_ansi_sequences
+from conftest import cstm_expect, remove_cariage, remove_ansi_sequences, get_exit_status
 
 
 def test_pipes():
@@ -17,11 +17,31 @@ def test_pipes():
         for line in bash.before.split("\n")[1:-1]
     ]
 
+    # bash_exit_status = get_exit_status(bash)
+
     bash.sendline("exit")
     bash.close()
-    # bash_export_output = get_bash_export_output(bash, export_cmd)
-    # bash.sendline("exit")
-    # bash.close()
+
+    minishell = pexpect.spawn("./minishell", encoding="utf-8")
+    cstm_expect(r"\$ ", minishell)
+    minishell.sendline(line)
+    cstm_expect(r"\$ ", minishell)
+
+    assert minishell.before is not None
+    minishell_output = [
+        remove_cariage(remove_ansi_sequences(line))
+        for line in minishell.before.split("\n")[1:-1]
+    ]
+
+    # minishell_exit_status = get_exit_status(minishell)
+
+    assert len(bash_output) == len(minishell_output)
+
+    minishell.sendline("exit")
+    minishell.close()
+    # minishell_export_output = get_minishell_export_output(minishell, export_cmd)
+    # minishell.sendline("exit")
+    # minishell.close()
 
     # minishell = pexpect.spawn("./minishell", encoding="utf-8")
     # for cmd in precommands:
@@ -31,4 +51,4 @@ def test_pipes():
     # minishell.sendline("exit")
     # minishell.close()
 
-    # assert_equal_export(bash_export_output, minishell_export_output)
+    # assert_equal_export(minishell_export_output, minishell_export_output)
