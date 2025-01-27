d3efbb42a303b982c311c50cc0490216afbab2a9
Kay Freyer <kfreyer@c1r6s4.42wolfsburg.de>
2025-01-27 11:30:05 +0100

    added test for no permission to execute

--- tests/end_to_end_tests/without_valgrind/test_minishell_errors.py
+++ tests/end_to_end_tests/without_valgrind/test_minishell_errors.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+from os.path import isfile
 from conftest import (
     start_process,
     parse_out_and_err_minishell,
@@ -57,4 +58,47 @@ def test_errors(cmd, err_msg, want_exit_status):
     assert len(stdout_minishell) == 1
     assert err_msg in stderr_minishell
     assert want_exit_status == int(stdout_minishell[0])
+    # assert_no_new_file_descriptors(open_fds_beginning, open_fds_end)
+
+
+#
+import os
+
+logname = os.getenv("LOGNAME")
+
+
+cmd = "echo $PATH\n"
+
+
+def test_errors_executables():
+
+    err_msg = "Permission denied"
+    want_exit_status = 1
+    if os.path.isfile(f"/home/{logname}/.local/bin/deleteme"):
+        os.remove(f"/home/{logname}/.local/bin/deleteme")
+    with open(f"/home/{logname}/.local/bin/deleteme", "w") as file:
+        file.write("Hello, World!")
+    os.chmod(
+        f"/home/{logname}/.local/bin/deleteme", 0o644
+    )  # Owner can read/write, others can only read
+
+    minishell = start_process("./minishell")
+    minishell = start_process("./minishell")
+    open_fds_beginning = get_open_fds()
+
+    cmd = "\n".join(["deleteme\n"] + ["echo $?\n"])
+    stdout_minishell, stderr_minishell, open_fds_end = (
+        send_cmds_minishell_with_open_fds(minishell, cmd)
+    )
+    stdout_minishell, stderr_minishell = parse_out_and_err_minishell(
+        stdout_minishell, stderr_minishell
+    )
+
+    assert_no_memory_error_fsanitize(stdout_minishell, stderr_minishell)
+    assert len(stderr_minishell) != 0
+    assert len(stdout_minishell) == 1
+    assert err_msg in stderr_minishell
+    assert want_exit_status == int(stdout_minishell[0])
+
+    os.remove(f"/home/{logname}/.local/bin/deleteme")
     # assert_no_new_file_descriptors(open_fds_beginning, open_fds_end)
