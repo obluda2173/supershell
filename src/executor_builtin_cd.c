16f8fec8ad66b3624edc82dbb3d6a64ed6bedbda
Erik An <erian@c3r1s1.42wolfsburg.de>
2025-01-28 12:03:33 +0100

    cleaned a bit cd builtin

--- src/executor_builtin_cd.c
+++ src/executor_builtin_cd.c
@@ -6,7 +6,7 @@
 /*   By: erian <erian@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 17:20:33 by erian             #+#    #+#             */
-/*   Updated: 2025/01/28 11:29:25 by erian            ###   ########.fr       */
+/*   Updated: 2025/01/28 12:02:36 by erian            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 t_env_var	*get_env_var(t_list *ep, char *key)
 {
-	t_list *tmp_ep;
-	t_env_var *tmp_var;
+	t_list		*tmp_ep;
+	t_env_var	*tmp_var;
 
 	tmp_ep = ep;
 	while (tmp_ep)
@@ -28,74 +28,77 @@ t_env_var	*get_env_var(t_list *ep, char *key)
 	return (NULL);
 }
 
-static void update_dirs(t_list **ep)
+static void	update_dirs(t_list **ep)
 {
-    char *tmp = getcwd(NULL, 0);
-    t_env_var *oldpwd = get_env_var(*ep, "OLDPWD");
-    t_env_var *pwd = get_env_var(*ep, "PWD");
+	char		*tmp;
+	t_env_var	*oldpwd;
+	t_env_var	*pwd;
 
-    if (!tmp)
-    {
-        perror("getcwd");
-        return ;
+	tmp = getcwd(NULL, 0);
+	oldpwd = get_env_var(*ep, "OLDPWD");
+	pwd = get_env_var(*ep, "PWD");
+	if (!tmp)
+	{
+		perror("getcwd");
+		return ;
 	}
-    if (oldpwd)
-    {
+	if (oldpwd)
+	{
 		free(oldpwd->value);
 		if (pwd && pwd->value)
 			oldpwd->value = ft_strdup(pwd->value);
 		else
 			oldpwd->value = ft_strdup(tmp);
-    }
-    if (pwd)
-    {
-        free(pwd->value);
-        pwd->value = tmp;
-    }
-    else
-        free(tmp); 
+	}
+	if (pwd)
+	{
+		free(pwd->value);
+		pwd->value = tmp;
+	}
+	else
+		free(tmp);
 }
 
-static int check_args(t_list *args)
+static int	check_args(t_list *args)
 {
-    if (ft_lstsize(args) > 1)
-    {
-        ft_putstr_fd("cd: too many arguments\n", STDERR_FILENO);
-        return (EXIT_FAILURE);
-    }
-    return (EXIT_SUCCESS);
+	if (ft_lstsize(args) > 1)
+	{
+		ft_putstr_fd("cd: too many arguments\n", STDERR_FILENO);
+		return (EXIT_FAILURE);
+	}
+	return (EXIT_SUCCESS);
 }
 
-int cstm_cd(t_list **ep, t_list *args)
+int	cstm_cd(t_list **ep, t_list *args)
 {
-    t_env_var *home;
+	t_env_var	*home;
 
-    if (check_args(args) != EXIT_SUCCESS)
-        return (EXIT_FAILURE);
-    if (!args)
-    {
-        home = get_env_var(*ep, "HOME");
-        if (!home || !home->value)
-        {
-            ft_putstr_fd("cd: HOME not set\n", STDERR_FILENO);
-            return (EXIT_FAILURE);
-        }
-        if (chdir(home->value) == -1)
-        {
-            ft_putstr_fd("cd: ", STDERR_FILENO);
-            perror(home->value);
-            return (EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        if (chdir(((t_argument *)args->content)->word) == -1)
-        {
-            ft_putstr_fd("cd: ", STDERR_FILENO);
-            perror(((t_argument *)args->content)->word);
-            return (EXIT_FAILURE);
-        }
-    }
-    update_dirs(ep);
-    return (EXIT_SUCCESS);
+	if (check_args(args) != EXIT_SUCCESS)
+		return (EXIT_FAILURE);
+	if (!args)
+	{
+		home = get_env_var(*ep, "HOME");
+		if (!home || !home->value)
+		{
+			ft_putstr_fd("cd: HOME not set\n", STDERR_FILENO);
+			return (EXIT_FAILURE);
+		}
+		if (chdir(home->value) == -1)
+		{
+			ft_putstr_fd("cd: ", STDERR_FILENO);
+			perror(home->value);
+			return (EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		if (chdir(((t_argument *)args->content)->word) == -1)
+		{
+			ft_putstr_fd("cd: ", STDERR_FILENO);
+			perror(((t_argument *)args->content)->word);
+			return (EXIT_FAILURE);
+		}
+	}
+	update_dirs(ep);
+	return (EXIT_SUCCESS);
 }
