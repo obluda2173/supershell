26afc3077cdfec9990f1c4e815f595b4146ea007
Erik An <erian@c3r1s1.42wolfsburg.de>
2025-01-28 16:38:41 +0100

    another silly mistake in exit (fixed exit status value)

--- src/executor_builtin_exit.c
+++ src/executor_builtin_exit.c
@@ -6,7 +6,7 @@
 /*   By: erian <erian@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/28 12:13:32 by erian             #+#    #+#             */
-/*   Updated: 2025/01/28 16:29:40 by erian            ###   ########.fr       */
+/*   Updated: 2025/01/28 16:35:39 by erian            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,26 +40,30 @@ int cstm_exit(t_list *args, t_data *data)
 {
     t_argument *arg;
     int exit_status = 0;
+    
+    ft_putstr_fd("exit\n", STDOUT_FILENO);
 
-    if (ft_lstsize(args) > 1)
+    if (args)
     {
         arg = (t_argument *)args->content;
         if (!valid_arg(arg->word))
         {
-            ft_putstr_fd("exit\nminishell: numeric argument required\n", STDERR_FILENO);
+            ft_putstr_fd("minishell: numeric argument required\n", STDERR_FILENO);
             data->exit = true;
-            exit_status = ft_atoi(arg->word) % 256;
-            if (exit_status < 0)
-                exit_status += 256;
+            return (2);
         }
-        else
+
+        exit_status = ft_atoi(arg->word) % 256;
+        if (exit_status < 0)
+            exit_status += 256;
+
+        if (ft_lstsize(args) > 1)
         {
-            ft_putstr_fd("exit\nexit: too many arguments\n", STDERR_FILENO);
+            ft_putstr_fd("exit: too many arguments\n", STDERR_FILENO);
             return (EXIT_FAILURE);
         }
     }
-    else
-        ft_putstr_fd("exit\n", STDOUT_FILENO);
+
     data->exit = true;
     return (exit_status);
 }
