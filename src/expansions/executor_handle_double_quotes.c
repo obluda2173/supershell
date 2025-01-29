127d5df0d1308d93c544b673b56cee3b3ad8cd29
Erik An <erian@c3r1s1.42wolfsburg.de>
2025-01-29 18:34:50 +0100

    norminette in double quotes is undefetable

--- src/expansions/executor_handle_double_quotes.c
+++ src/expansions/executor_handle_double_quotes.c
@@ -6,58 +6,87 @@
 /*   By: erian <erian@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/10 15:57:45 by erian             #+#    #+#             */
-/*   Updated: 2025/01/12 13:03:31 by erian            ###   ########.fr       */
+/*   Updated: 2025/01/29 18:32:22 by erian            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "executor.h"
 
-char    *handle_double_quotes(const char *word,  t_data *data)
+static char	*process_exit_status(t_data *data, char **result,
+			size_t *result_len, size_t *i)
 {
-	size_t len = ft_strlen(word);
-	char *result = malloc(1);
-	size_t result_len = 0;
-	size_t i = 0;
+	char	*exit_status_str = ft_itoa(data->exit_status);
+	size_t	exit_status_len;
 
+	if (!exit_status_str)
+		return (NULL);
+	exit_status_len = ft_strlen(exit_status_str);
+	*result = append_to_result(*result, result_len, exit_status_str, exit_status_len);
+	free(exit_status_str);
+	if (!*result)
+		return (NULL);
+	(*i)++;
+	return (*result);
+}
+
+static char	*process_dollar_var(char *dollar_expansion, char **result,
+			size_t *result_len)
+{
+	size_t	expansion_len;
+
+	if (!dollar_expansion)
+		return (NULL);
+	expansion_len = ft_strlen(dollar_expansion);
+	*result = append_to_result(*result, result_len, dollar_expansion, expansion_len);
+	free(dollar_expansion);
+	if (!*result)
+		return (NULL);
+	return (*result);
+}
+
+static char	*process_normal_char(const char *word, size_t *i,
+			char **result, size_t *result_len)
+{
+	char temp[2] = {word[(*i)++], '\0'};
+	*result = append_to_result(*result, result_len, temp, 1);
+	return (*result);
+}
+
+char	*handle_double_quotes(const char *word, t_data *data)
+{
+	size_t	len;
+	char	*result;
+	size_t	result_len;
+	size_t	i;
+	char	*dollar_expansion;
+
+	result = malloc(1);
 	if (!result)
-		return NULL;
-
+		return (NULL);
+	result_len = 0;
+	i = 0;
+	len = ft_strlen(word);
 	result[0] = '\0';
-
+	
 	while (i < len && word[i] != '\"')
 	{
 		if (word[i] == '$')
 		{
 			i++;
-			if (word[i] == '?')
-			{
-				char *exit_status_str = ft_itoa(data->exit_status);
-				size_t exit_status_len = ft_strlen(exit_status_str);
-				result = append_to_result(result, &result_len, exit_status_str, exit_status_len);
-				free(exit_status_str);
-				if (!result)
-					return NULL;
-				i++;
-			}
+			if (i < len && word[i] == '?')
+				result = process_exit_status(data, &result, &result_len, &i);
 			else
 			{
-				char *dollar_expansion = handle_dollar(&word[i], data);
-				size_t expansion_len = ft_strlen(dollar_expansion);
-				result = append_to_result(result, &result_len, dollar_expansion, expansion_len);
-				free(dollar_expansion);
-				if (!result)
-					return NULL;
+				dollar_expansion = handle_dollar(&word[i], data);
+				result = process_dollar_var(dollar_expansion, &result, &result_len);
 				while (word[i] && (ft_isalnum(word[i]) || word[i] == '_'))
-					i++;
+					(i)++;
 			}
 		}
 		else
-		{
-			char temp[2] = {word[i++], '\0'};
-			result = append_to_result(result, &result_len, temp, 1);
-			if (!result)
-				return NULL;
-		}
+			result = process_normal_char(word, &i, &result, &result_len);
+		if (!result)
+			return (NULL);
 	}
 	return (result);
 }
